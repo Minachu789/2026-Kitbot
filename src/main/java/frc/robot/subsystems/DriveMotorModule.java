3270396fa5b4ee2c6ec4d0843de83158db8e816e
Mina <minachu3344@gmail.com>
2026-06-05 19:44:18 +0800

    optimize

--- src/main/java/frc/robot/subsystems/DriveMotorModule.java
+++ src/main/java/frc/robot/subsystems/DriveMotorModule.java
@@ -1,33 +1,33 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import frc.robot.lib.IDashBoardProvider;
 
-public class DriveMotorModule implements IDashBoardProvider{
+public class DriveMotorModule implements IDashBoardProvider {
     private final SparkMax frontSpark;
     private final SparkMax backSpark;
 
-    public DriveMotorModule(int frontID, int backID, Boolean frontReverse, Boolean backReverse){
+    public DriveMotorModule(int frontID, int backID, Boolean frontReverse, Boolean backReverse) {
         this.registerDashbroad();
-        this.frontSpark = new SparkMax(frontID,MotorType.kBrushed);
-        this.backSpark = new SparkMax(backID,MotorType.kBrushed);
+        this.frontSpark = new SparkMax(frontID, MotorType.kBrushed);
+        this.backSpark = new SparkMax(backID, MotorType.kBrushed);
         SparkMaxConfig frontConfig = new SparkMaxConfig();
         frontConfig
-            .idleMode(IdleMode.kCoast)
-            .inverted(frontReverse)
-            .smartCurrentLimit(40);
+                .idleMode(IdleMode.kCoast)
+                .inverted(frontReverse)
+                .smartCurrentLimit(40);
 
         SparkMaxConfig backConfig = new SparkMaxConfig();
         backConfig
-            .idleMode(IdleMode.kCoast)
-            .inverted(backReverse)
-            .smartCurrentLimit(40);
+                .idleMode(IdleMode.kCoast)
+                .inverted(backReverse)
+                .smartCurrentLimit(40);
         this.frontSpark.configure(frontConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         this.backSpark.configure(backConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
@@ -44,6 +44,6 @@ public class DriveMotorModule implements IDashBoardProvider{
 
     @Override
     public void putDashboard() {
-        
+
     }
 }
